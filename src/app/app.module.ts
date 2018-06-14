45b44e695c0e1bc10019cf21a0e37b37ef8c7027
Samir Husak <samhusak@hotmail.com>
2018-06-14 12:00:05 +0200

    Shared components created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { InfoComponent } from './shared components/info/info.component';
+import { HeaderComponent } from './shared components/header/header.component';
+import { FooterComponent } from './shared components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InfoComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
