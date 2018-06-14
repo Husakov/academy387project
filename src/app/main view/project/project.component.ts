f1ab2d45665d80a39373ca17b2297df79341dd56
sadin97 <32278184+sadin97@users.noreply.github.com>
2018-06-14 20:04:52 +0200

    editing project component

--- src/app/main view/project/project.component.ts
+++ src/app/main view/project/project.component.ts
@@ -7,9 +7,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProjectComponent implements OnInit {
 
+  currentTab = "Active";
+
+  projects = [
+    {
+      id: 1,
+      name: "Project 1",
+      desc: "Ovo je opis prvog projekta.",
+      status: "Active"
+    },
+    {
+      id: 2,
+      name: "Project 2",
+      desc: "Ovo je opis drugo projekta. Lorem ipsum, lorem ipsum, lorem ipsum",
+      status: "Archive"
+    };
+  ]
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  createProject() {
+    let newID = this.projects.length + 1;
+    this.projects.push({id: newID, name: "Project " + newID, desc: "", status: "Active"});
+
   }
 
 }
